63a29a54fd6ee7bb20f090f0c9d77d952bde1673
Əli <liyevli06@gmail.com>
2026-01-23 12:50:35 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="az">
-      <body>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
